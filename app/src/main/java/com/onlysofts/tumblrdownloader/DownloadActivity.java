f3c3cd316be3a8c602bbc04495906b2a60d71845
hanqiyg <hanqiyg@gmail.com>
2017-06-01 17:02:46 +0800

    st

--- app/src/main/java/com/onlysofts/tumblrdownloader/DownloadActivity.java
+++ app/src/main/java/com/onlysofts/tumblrdownloader/DownloadActivity.java
@@ -5,10 +5,16 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by i.c.e on 2017/6/1.
@@ -17,6 +23,7 @@ import android.widget.ListView;
 public class DownloadActivity extends AppCompatActivity {
     public Button add = null;
     public ListView listView = null;
+    private List<Map<String, Object>> listData;
     public String[] urls =
     {
 
@@ -37,14 +44,42 @@ public class DownloadActivity extends AppCompatActivity {
                 }
             }
         });
+        listData = getData();
         listView = (ListView) findViewById(R.id.listview);
-        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.download_info, urls);
-        listView.setAdapter(adapter);
+      }
+
+    private List<Map<String,Object>> getData() {
+        return null;
     }
+
     public class MHandler extends Handler{
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
         }
     }
+
+    public class DownloadListAdapter extends BaseAdapter{
+        private LayoutInflater mInflater;
+        @Override
+        public int getCount() {
+            return listData.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            //http://www.cnblogs.com/allin/archive/2010/05/11/1732200.html
+            return convertView;
+        }
+     }
 }
